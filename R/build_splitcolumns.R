6b898a92605a04cdc0a980b9780bdb60feb14dcb
fmichelsendis <franco.michelsendis@gmail.com>
2018-11-05 01:02:46 +0100

    fixing functions

--- R/build_splitcolumns.R
+++ R/build_splitcolumns.R
@@ -20,12 +20,15 @@ build_splitcolumns<- function(df){
     
     df$FULLIDA<-df$FULLID
     df<-cSplit(as.data.table(df), "FULLIDA", "-")
-    setnames(df, old=c("FULLIDA_1","FULLIDA_2", "FULLIDA_3"), 
-             new=c("FISS", "FORM", "SPEC"))
-    df$FULLIDA_4<-df$FULLIDA_5<-NULL
-    df<-df%>%mutate(CASETYPE = paste0(.data$FISS,"-",.data$FORM,"-",
-                                                .data$SPEC))
-    
+    setnames(df, old=c("FULLIDA_1","FULLIDA_2", "FULLIDA_3", "FULLIDA_4", "FULLIDA_5"), 
+             new=c("FISS", "FORM", "SPEC", "CASE", "SUBCASE"))
+    #df$FULLIDA_4<-df$FULLIDA_5<-NULL
+    df<-df%>%mutate(CASETYPE = paste0(.data$FISS,"-",.data$FORM,"-",.data$SPEC),
+                    SHORTID = paste0(substring(.data$FISS, 1, 1),
+                                     substring(.data$FORM, 1, 1),
+                                     substring(.data$SPEC, 1, 1),"-",
+                                     .data$CASE,"-",.data$SUBCASE)
+                      
+                    )
   return(df)
- 
 }
--- R/return_discrepant.R
+++ R/return_discrepant.R
@@ -10,14 +10,14 @@ return_discrepant<- function(df, INST1, INST2, sigma_treshold = 0){
 
 inst1<-filter(df, .data$INST==INST1)
 inst2<-filter(df, .data$INST==INST2)
-m<-merge(inst1, inst2, by = c("FULLID", "FISS", "FORM", "SPEC", "MODEL", "EALF", "LIBVER"))%>%
-  select("FULLID", "FISS", "FORM", "SPEC", "MODEL", "EALF", "LIBVER", 
+m<-merge(inst1, inst2, by = c("FULLID", "SHORTID", "MODEL", "EALF", "LIBVER"))%>%
+  select("SHORTID", "FULLID", "MODEL", "EALF", "LIBVER", 
          "CODE.x", "CALCVAL.x", "CALCERR.x", 
          "CODE.y", "CALCVAL.y", "CALCERR.y")%>%
   mutate(
-    DELTA_PCM = (.data$CALCVAL.y/.data$CALCVAL.x - 1) *1e+5,
-    TOT_STAT_ERR_PCM = sqrt(.data$CALCERR.x^2 + .data$CALCERR.y^2) * 1e+5,
-    DELTA_SIGMA_STAT = .data$DELTA_PCM/.data$TOT_STAT_ERR_PCM
+    DELTA_PCM = round((.data$CALCVAL.y/.data$CALCVAL.x - 1) *1e+5,0),
+    TOT_STAT_ERR_PCM = round(sqrt(.data$CALCERR.x^2 + .data$CALCERR.y^2) * 1e+5,0),
+    DELTA_SIGMA_STAT = round(.data$DELTA_PCM/.data$TOT_STAT_ERR_PCM, 1)
          )
 return(filter(m, abs(.data$DELTA_SIGMA_STAT)>sigma_treshold))
 }
