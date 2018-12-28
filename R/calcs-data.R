d983451899c87757720df36c45dcc68d48224b78
fmichelsendis <franco.michelsendis@gmail.com>
2018-12-28 21:25:35 +0100

    fix definition of calcs data

--- R/calcs-data.R
+++ R/calcs-data.R
@@ -1,4 +1,14 @@
-#' @format A dataset of calculated results with 13282 observations of 7 variables:
+#' A dataset of benchmark calculations.
+#' 
+#' The structure of this dataset (7 features per observation) 
+#' is the minimal template for calculated results 
+#' to be imported into the sendis package.
+#' 
+#' @name calcs
+#' @docType data
+#' @usage data(calcs)
+#' @format A dataset of calculated results with 13282 observations of 7 variables
+#' 
 #' \describe{
 #'   \item{FULLID}{Full Identifier of Benchmark case and subcase}
 #'   \item{MODEL}{Model in case different models exist for same benchmark}
